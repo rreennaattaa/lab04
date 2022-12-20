091bc1cf6c084eda72c86b0bbc90fde8f16b08ad
Mikhailova Renata <renata.mikhailova@gmail.com>
2022-12-20 18:39:34 +0300

    Create web app

---
+++ MyWebApp/app.py
@@ -0,0 +1,39 @@
+import requests
+from flask import Flask, render_template, request
+import psycopg2
+
+
+app = Flask(__name__)
+conn = psycopg2.connect(database="service_db",
+	user="postgres",
+	password="admin123",
+	host="localhost",
+	port="5432"
+)
+cursor = conn.cursor()
+
+
+@app.route('/login/', methods=['GET'])
+def index():
+	return render_template('login.html')
+
+
+@app.route('/login/', methods=['POST'])
+def login():
+	username = request.form.get('username')
+	password = request.form.get('password')
+	if username.strip() and password.strip():
+		cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
+		records = list(cursor.fetchall())
+		if records:
+			return render_template(
+				'account.html',
+				full_name=records[0][1],
+				username=records[0][2],
+				password=records[0][3],
+			)
+	return render_template('login.html')
+
+
+if __name__ == '__main__':
+	app.run("localhost", port=5000, debug=True)
